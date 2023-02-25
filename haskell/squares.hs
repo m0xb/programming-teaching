72e8d8ed358cb8c85c15e6e87352324b82b45000
Steven Mitchell <mitchellxb@gmail.com>
2023-02-24 19:50:01 -0700

    Update squares.hs to handle multiple squares. Still hardcodes canvas size.

--- haskell/squares.hs
+++ haskell/squares.hs
@@ -1,52 +1,47 @@
-
 type Square = (Int,Int,Int,[Char])
 
 drawSquares :: [Square] -> IO ()
-drawSquares _ = putStr result
+drawSquares squares = putStr result
   where
-    result = ".....\n.+++.\n.+-+.\n.+++.\n.....\n"
-
-drawSquare :: Square -> IO ()
-drawSquare (sx, sy, size, [b, f]) = putStr result
-  where
-    -- result = render (sx, sy, size, [b, f]) (size * size - 1) size
-    result = render (sx, sy, size, [b, f]) 96 12
+    result = render squares (1000-1) 50
 
 -- render square idx rowLength
-render :: Square -> Int -> Int -> [Char]
--- render (sx, sz, size, [b, f]) idx rowLength =
-render square idx _ | idx < 0 = ""
-render square idx rowLength = init ++ cur : nl
+render :: [Square] -> Int -> Int -> [Char]
+render squares idx _ | idx < 0 = ""
+render squares idx rowLength = init ++ cur : nl
   where
     x = mod idx rowLength
     y = div idx rowLength
-    init = (render square (idx - 1) rowLength)
+    init = (render squares (idx - 1) rowLength)
     nl = if x == rowLength - 1 then "\n" else ""
-    cur = if isOnBorder square x y then getBorderChar square else '.'
+    cur = sample squares x y
 -- render width*height-1
 -- render 3 "4\n"
 -- render 2 "3"
 -- render 1 "2\n"
 -- render 0 "1"
 
-isOnBorder :: Square -> Int -> Int -> Bool
-isOnBorder (sx, sy, size, _) x y = isTop || isBottom || isLeft || isRight
+-- samples the character to render at a single x,y position
+-- uses '.' as the background character
+sample :: [Square] -> Int -> Int -> Char
+sample squares x y = sampleWithDefault squares '.' x y
+
+-- same as sample, but
+sampleWithDefault :: [Square] -> Char -> Int -> Int -> Char
+sampleWithDefault [] bg _ _ = bg
+sampleWithDefault (square:rest) bg x y = sampleWithDefault rest ch x y
   where
-    isLeft = x == sx && y >= sy && y < sy + size
-    isRight = x == sx + size - 1 && y >= sy && y < sy + size
-    isTop = y == sy && x >= sx && x < sx + size
-    isBottom = y == sy + size - 1 && x >= sx && x < sx + size
+    sqch = sampleSquare square x y
+    ch = if sqch == '\0' then bg else sqch
 
---isOnBorder :: Square -> Int -> Int -> Bool
---isOnBorder (sx, sy, size, _) x y
---  | x == sx && y >= sy && y < sy + size = True
---  | x == sx + size - 1 && y >= sy && y < sy + size = True
---  | y == sy && x >= sx && x < sx + size = True
---  | y == sy + size - 1 && x >= sx && x < sx + size = True
---  | otherwise = False
-
-getBorderChar :: Square -> Char
-getBorderChar (_, _, _, [b, _]) = b
-
-getFillChar :: Square -> Char
-getFillChar (_, _, _, [_, f]) = f
+-- returns NUL for transparent
+sampleSquare :: Square -> Int -> Int -> Char
+sampleSquare (sx, sy, size, [b, f]) x y
+  | inBounds && x == sx = b
+  | inBounds && x == sx + size - 1 = b
+  | inBounds && y == sy = b
+  | inBounds && y == sy + size - 1 = b
+  | inBounds = if f == '#' then '\0' else f
+  | otherwise = '\0'
+  where
+    inBounds = y >= sy && y < sy + size && x >= sx && x < sx + size
