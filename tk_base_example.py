69d87dc2c0c48346317d359b2db74291d15729e0
Steven Mitchell <mitchellxb@gmail.com>
2023-03-17 01:30:01 -0700

    Add tk_base_example.py

---
+++ tk_base_example.py
@@ -0,0 +1,12 @@
+import random
+import tk_base
+
+def draw(app):
+    # Draw a randomly sized and positioned blue rectangle
+    x = random.randint(1, 800)
+    y = random.randint(1, 600)
+    w = random.randint(1, 100)
+    h = random.randint(1, 100)
+    app.canvas.create_rectangle(x, y, x+w, y+h, fill="#00F", outline="#44F")
+
+tk_base.TkBaseApp({"draw": draw}).run()
