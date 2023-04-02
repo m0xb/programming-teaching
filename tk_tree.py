1082620b3f43e729aacd32fba15e647f90d1101d
Steven Mitchell <mitchellxb@gmail.com>
2023-04-02 00:49:04 -0700

    tk_tree.py: add rotations, display previous tree, start of balanced insert

--- tk_tree.py
+++ tk_tree.py
@@ -9,11 +9,13 @@ import tk_base
 class DrawOptions:
     line_colors: list
     subtree_bounds: bool = False
+    text_color_fn: any = None  # apparently if I don't add a type annotation here, dataclasses doesn't see it as an attribute?
 
 
 class TreeNode:
 
-    NODE_SIZE = 30
+    NODE_SIZE = 40
+    FONT_SIZE = 20
     H_PAD = 5
     V_PAD = 5
 
@@ -26,6 +28,19 @@ class TreeNode:
         self.count = 1
         self.left = left
         self.right = right
+
+    def clone(self):
+        # call clone() on the left subtree to recursively clone it, if it is not none
+        new_left = self.left.clone() if self.left else None
+        # call clone() on the right subtree to recursively clone it, if it is not none
+        new_right = self.right.clone() if self.right else None
+        # create a new TreeNode which is the clone of self, and pass the left + right clones into it
+        new_self = TreeNode(self.value, new_left, new_right)
+        # and also copy the `count` variable over
+        new_self.count = self.count
+        # ... no need to clone x/y/width/height since they are recomputed in draw()
+        # return the recursively cloned self
+        return new_self
 
     def draw(self, canvas, opts: DrawOptions):
         self.calculate_size()
@@ -73,10 +88,12 @@ class TreeNode:
 
         line_color = opts.line_colors[(self.tree_height() - 1) % len(opts.line_colors)]
 
+        text_color = opts.text_color_fn(self) if opts.text_color_fn else '#FF0000'
+
         canvas.create_oval(self.x + half_width - half_size, self.y, self.x + half_width + half_size, self.y + self.NODE_SIZE, outline=line_color)
-        canvas.create_text(self.x + half_width, self.y + half_size, text=str(self.value))
+        canvas.create_text(self.x + half_width, self.y + half_size, text=str(self.value), fill=text_color, font=('serif', self.FONT_SIZE))
         if self.count > 1:
-            canvas.create_text(self.x + half_width, self.y + half_size + 10, text=str(self.count), font=('serif', 8))
+            canvas.create_text(self.x + half_width, self.y + half_size + int(self.FONT_SIZE * .6), text=str(self.count), fill=text_color, font=('serif', self.FONT_SIZE//2))
         if opts.subtree_bounds:
             canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, outline=line_color)
         if self.left:
@@ -100,6 +117,67 @@ class TreeNode:
             else:
                 self.right = node
 
+    def balance(self):
+        hl = self.left.tree_height() if self.left else 0
+        hr = self.right.tree_height() if self.right else 0
+        return hr - hl
+
+    def balanced_insert(self, v):
+        if v == self.value:
+            self.count += 1
+            return
+        balance = self.balance()
+        if balance == 0:
+            if v < self.value:
+                if self.left:
+                    self.left.balanced_insert(v)
+                else:
+                    self.left = TreeNode(v)
+            else:
+                if self.right:
+                    self.right.balanced_insert(v)
+                else:
+                    self.right = TreeNode(v)
+        elif balance < 0:
+            if v < self.value:
+                # rotate right, insert left
+                pass
+            else:
+                if self.right:
+                    self.right.balanced_insert(v)
+                else:
+                    self.right = TreeNode(v)
+        else:  # balance > 0
+            if v < self.value:
+                if self.left:
+                    self.left.balanced_insert(v)
+                else:
+                    self.left = TreeNode(v)
+            else:
+                # no need to nullcheck self.right
+                # since we are leaning right, there _must_ be a self.right
+                assert self.right
+                new_self = self.rotate_left()
+                new_self.balanced_insert(v)
+                # rotate left, insert right
+                pass
+
+    def rotate_left(self):
+        b_temp = self
+        a_left_temp = self.right.left
+        root = self.right  # B = A
+        root.left = b_temp
+        root.left.right = a_left_temp
+        return root
+
+    def rotate_right(self):
+        b_temp = self
+        a_right_temp = self.left.right
+        root = self.left  # B = A
+        root.right = b_temp
+        root.right.left = a_right_temp
+        return root
+
     def tree_height(self):
         return max(
             self.left.tree_height() if self.left else 0,
@@ -109,12 +187,29 @@ class TreeNode:
 
 class TreeUI:
     def __init__(self):
-        self.draw_options = DrawOptions(['#FFF'], subtree_bounds=False)
+        def text_color_fn(node):
+            if node.value == 20:
+                return '#4040FF'
+            elif node.value == 15:
+                return '#8080FF'
+            elif node.value == 10:
+                return '#FF0000'
+            elif node.value == 5:
+                return '#FF8800'
+            elif node.value == 1:
+                return '#FFFF00'
+            else:
+                return '#A0A0A0'
+        self.draw_options = DrawOptions(['#FFF'], subtree_bounds=False, text_color_fn=text_color_fn)
         self.colorful = False
         self.reset()
 
     def reset(self, app=None):
-        self.tree = TreeNode(50)
+        #self.tree = TreeNode(50)
+        self.tree = TreeNode(10, # B
+                             TreeNode(5, TreeNode(1), TreeNode(7)),  # A + children
+                             TreeNode(15, TreeNode(12), TreeNode(20)))  # B.right
+        self.prev_tree = None
         self.tree.x = 5
         self.tree.y = 5
         self.next_value = 1
@@ -134,9 +229,22 @@ class TreeUI:
         self.draw(app)
 
     def add_node(self, app):
+        self.prev_tree = self.tree.clone()
         self.tree.insert(TreeNode(random.randint(1, 100)))
         # self.tree.insert(TreeNode(self.next_value))
+        # self.tree.insert(TreeNode([1, 74, 47][self.next_value - 1]))
+        # self.tree.insert(TreeNode([52, 54, 56, 58, 49, 51, 53, 55, 57, 59][self.next_value - 1]))
         self.next_value += 1
+        self.draw(app)
+
+    def rotate_right(self, app):
+        self.prev_tree = self.tree.clone()
+        self.tree = self.tree.rotate_right()
+        self.draw(app)
+
+    def rotate_left(self, app):
+        self.prev_tree = self.tree.clone()
+        self.tree = self.tree.rotate_left()
         self.draw(app)
 
     def draw(self, app):
@@ -148,10 +256,24 @@ class TreeUI:
         # tree.y = 5
 
         app.canvas.delete("all")
+        self.tree.x = 5
+        self.tree.y = 5
         self.tree.draw(app.canvas, self.draw_options)
+        if self.prev_tree:
+            self.prev_tree.x = self.tree.x + self.tree.width + 25
+            self.prev_tree.y = self.tree.y
+            self.prev_tree.draw(app.canvas, DrawOptions(['#888'], text_color_fn=self.draw_options.text_color_fn))
 
         app.canvas.create_text(5, 800, anchor=tk.SW, fill="#FFF", text="Height: " + str(self.tree.tree_height()))
+        app.canvas.create_text(100, 800, anchor=tk.SW, fill="#FFF", text="Balance: " + str(self.tree.balance()))
 
 
 tree_ui = TreeUI()
-tk_base.TkBaseApp({"Add Node": tree_ui.add_node, "Toggle Bounds": tree_ui.toggle_bounds, "Toggle Colors": tree_ui.toggle_colorful, "Reset": tree_ui.reset}).run()
+tk_base.TkBaseApp({
+    "Add Node": tree_ui.add_node,
+    "Rotate Left": tree_ui.rotate_left,
+    "Rotate Right": tree_ui.rotate_right,
+    "Toggle Bounds": tree_ui.toggle_bounds,
+    "Toggle Colors": tree_ui.toggle_colorful,
+    "Reset": tree_ui.reset,
+}).run()
